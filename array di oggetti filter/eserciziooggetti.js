b6e32c0216ba0e885dc536ad17e727eed886c86c
matteo2302 <matteo.pizzichemi01@gmail.com>
2026-08-17 04:09:51 +0200

    Add initial implementation of eserciziooggetti.js and landingpage.html

---
+++ array di oggetti filter/eserciziooggetti.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+let giornidilavoro = [
+    { lavorato: true, giorno: "lunedi", ore: 3, ruolo: "bibite" },
+    { lavorato: true, giorno: "martedi", ore: 4, ruolo: "lavaggio" },
+    { lavorato: false, giorno: "mercoledi", ore: 3.5, ruolo: "bibite" },
+    { lavorato: true, giorno: "giovedi", ore: 0, ruolo: "nessuno" }
+];
+let settimana = giornidilavoro.map((giornodilavoro) => {
+    return giornodilavoro.giorno
+});
+console.log(settimana);
+
+let lavoro = giornidilavoro.map((giornodilavoro) => { return { lavorato: giornodilavoro.lavorato, giorno: giornodilavoro.giorno } });
+
+console.log(lavoro)
