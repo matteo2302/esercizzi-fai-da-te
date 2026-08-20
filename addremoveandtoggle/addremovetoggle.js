bf92a7e805394cb6a350b53a1f2c59765d35a493
matteo2302 <matteo.pizzichemi01@gmail.com>
2026-08-20 22:35:27 +0200

    Add unique IDs to game objects and improve logging in addremovetoggle.js

--- addremoveandtoggle/addremovetoggle.js
+++ addremoveandtoggle/addremovetoggle.js
@@ -1,13 +1,36 @@
 let games = [
-    { name: "darksouls", year: 2010, difficulty: "easy", completed: true },
-    { name: "darksouls2", year: 2015, difficulty: "hard", completed: true },
-    { name: "darksouls3", year: 2017, difficulty: "medium", completed: true },
+    { id: 1, name: "darksouls", year: 2010, difficulty: "easy", completed: true },
+    { id: 2, name: "darksouls2", year: 2015, difficulty: "hard", completed: true },
+    { id: 3, name: "darksouls3", year: 2017, difficulty: "medium", completed: true },
 
 ]
-let newGame = [{ name: "bloodborne", year: 2014, difficulty: "very hard", completed: true }];
+console.log(games)
+
+
+
+//add to a new array without mutating the original
+
+let newGame = [{ id: 4, name: "bloodborne", year: 2014, difficulty: "very hard", completed: true }];
 function addGame(games, newGame) {
     return games.concat(newGame)
 }
 let newGameList = addGame(games, newGame)
+console.log(newGameList);
+
+
+
+// removing an item from an array
+
+/*function deleteGame(newGameList, 1) {
+    return newGameList.slice(1);
+}
+
+newGameList = deleteGame(newGameList, 1);*/
+
+function deleteGame(newGameList) {
+    return newGameList.filter(game => game.id !== 2)
+
+}
+
+newGameList = deleteGame(newGameList)
 console.log(newGameList)
-console.log(games)
